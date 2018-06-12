626e2671374529dcae718ebaed6274ae5fc0c7a9
DLPerini <daniela.perini123@gmail.com>
2018-06-11 17:43:27 -0700

    change to data (APIs)

--- docs/README.txt
+++ docs/README.txt
@@ -8,3 +8,7 @@ These Charts are the easiest to display the data I had.
 The Area chart is great to visualize the rise of numbers and it shows the numbers precisely but it makes it a little hard to see which was the lowest and which was the highest year in electricity consumption.
 The Column chart makes it easy to visualize how the production of renewale energy changed during the years, and you can easily see that the year of 1996 was the highest one and 1961 the lowest, thanks to the separated bars, but the numbers showed are not precise.
 The Line chart is, in my opinion, the best of the three. It shows the numbers precisely and it is easy to see which year was the highest and which was the lowest. And it makes it easy to compare all the data I had.
+
+Data was taken from: 
+https://www.eia.gov/opendata/qb.php?category=40236&sdid=SEDS.TETCB.FL.A
+https://www.eia.gov/opendata/qb.php?category=40426&sdid=SEDS.REPRB.FL.A
--- script.js
+++ script.js
@@ -1,24 +1,15 @@
 google.charts.load('current', {'packages':['corechart', 'line', 'bar']});
 
-google.charts.setOnLoadCallback(drawConsumptionChart);
-google.charts.setOnLoadCallback(drawProductionChart);
+      google.charts.setOnLoadCallback(getData);
+      google.charts.setOnLoadCallback(getData2);
 google.charts.setOnLoadCallback(drawComparingChart);
 
 
-function drawConsumptionChart() {
-        var data = google.visualization.arrayToDataTable([
-          ['Year', 'Billion Btu'],
-         ['1960', 57344],  ['1961', 62705],   ['1962', 71216],  ['1963', 78258],   ['1964', 87016],  ['1965', 95878],
-        ['1966', 108610],   ['1967', 119314],  ['1968', 135509],  ['1969', 153032],  ['1970', 171346], ['1971', 188564],
-        ['1972', 209665], ['1973', 237104], ['1974', 235482], ['1975', 242096], ['1976', 252408], ['1977', 270767],
-        ['1978', 289031], ['1979', 295551], ['1980', 309694], ['1981', 317921], ['1982', 315744], ['1983', 329216],
-        ['1984', 353246], ['1985', 379307], ['1986', 398095], ['1987', 417862], ['1988', 444382], ['1989', 472473],
-        ['1990', 489741], ['1991', 499299], ['1992', 501598], ['1993', 521176], ['1994', 544365], ['1995', 571483],
-        ['1996', 586291], ['1997', 597240], ['1998', 639254], ['1999', 638966], ['2000', 668216], ['2001', 684966],
-        ['2002', 718136], ['2003', 741696], ['2004', 745810], ['2005', 767622], ['2006', 778685], ['2007', 788461],
-        ['2008', 771702], ['2009', 766848], ['2010', 788887], ['2011', 768009], ['2012', 752941], ['2013', 757189],
-        ['2014', 771379], ['2015', 803865], ['2016', 804283]
-          ]);
+function drawBasic2(freshData) {
+        console.log("freshData", freshData)
+        freshData.unshift(["Year", "Billion BTUs"])
+
+        var data = google.visualization.arrayToDataTable(freshData);
 
         var options = {
           title: 'Electricity total consumption (i.e. sold) Florida',
@@ -27,43 +18,85 @@ function drawConsumptionChart() {
           colors: ['#CC0000']
         };
 
-        var chart = new google.visualization.AreaChart(document.getElementById('conumption_chart'));
+        var chart = new google.visualization.AreaChart(document.getElementById('consumption_chart'));
+
         chart.draw(data, options);
       }
 
-function drawProductionChart() {
-     var data = new google.visualization.arrayToDataTable([
-          ['Year', 'Billion Btu'],
-['1960', 35680],  ['1961', 34417],   ['1962', 35826],  ['1963', 38613],   ['1964', 39016],  ['1965', 39916],
-        ['1966', 42760],   ['1967', 44550],  ['1968', 49556],  ['1969', 51489],  ['1970', 51035], ['1971', 49968],
-        ['1972', 54389], ['1973', 56250], ['1974', 52429], ['1975', 50007], ['1976', 56507], ['1977', 59953],
-        ['1978', 65357], ['1979', 69419], ['1980', 90049], ['1981', 83117], ['1982', 104674], ['1983', 91705],
-        ['1984', 108740], ['1985', 110698], ['1986', 116356], ['1987', 107606], ['1988', 113802], ['1989', 232261],
-        ['1990', 198986], ['1991', 212955], ['1992', 230779], ['1993', 217028], ['1994', 215563], ['1995', 220211],
-        ['1996', 240343], ['1997', 231308], ['1998', 205485], ['1999', 204114], ['2000', 194952], ['2001', 158038],
-        ['2002', 174327], ['2003', 188473], ['2004', 179462], ['2005', 183175], ['2006', 185564], ['2007', 190489],
-        ['2008', 195232], ['2009', 213642], ['2010', 223518], ['2011', 222956], ['2012', 220020], ['2013', 229666],
-        ['2014', 226863], ['2015', 234192]
-        ]);
+
+function getData2(){
+        // Create a new request object
+        let request = new XMLHttpRequest()
+        // TODO: URL to contact goes here
+        let requestUrl = "https://api.eia.gov/series/?api_key=43fd391551b1a57ac02073fb37571ca7&series_id=SEDS.TETCB.FL.A"
+        // Open a connection
+        request.open('GET', requestUrl, true)
+        // Callback for when the request completes
+        request.onload = function(){
+          let theActualData = JSON.parse(request.response).series[0].data
+
+          drawBasic2(theActualData)
+        }
+        // Callback for when there's an error
+        request.error = function(err){
+          console.log("error is: ", err)
+        }
+        // Send the request to the specified URL
+        request.send()
+      }
+
+
+
+
+
+function drawBasic(freshData) {
+        console.log("freshData", freshData)
+        freshData.unshift(["Year", "Billion BTUs"])
+
+        var data = google.visualization.arrayToDataTable(freshData);
 
         var options = {
-          width: 800,
-          legend: { position: 'none' },
-          chart: {
-            title: 'Renewable energy production Florida',
-            subtitle: 'Billion Btu' },
-          axes: {
-            x: {
-              0: { side: 'top'}
-            }
-          },
-          bar: { groupWidth: "80%" },
-          colors: ['#009900']
+            width: 800,
+            legend: { position: 'none' },
+            chart: {
+              title: 'Renewable energy production Florida',
+              subtitle: 'Billion Btu' },
+            axes: {
+              x: {
+                0: { side: 'top'}
+              }
+            },
+            bar: { groupWidth: "80%" },
+            colors: ['#009900']
         };
-          var chart = new google.charts.Bar(document.getElementById('production_chart'));
-        // Convert the Classic options to Material options.
-        chart.draw(data, google.charts.Bar.convertOptions(options));
-}
+
+        var chart = new google.charts.Bar(document.getElementById('production_chart'));
+
+        chart.draw(data, options);
+      }
+
+
+function getData(){
+        // Create a new request object
+        let request = new XMLHttpRequest()
+        // TODO: URL to contact goes here
+        let requestUrl = "https://api.eia.gov/series/?api_key=43fd391551b1a57ac02073fb37571ca7&series_id=SEDS.REPRB.FL.A"
+        // Open a connection
+        request.open('GET', requestUrl, true)
+        // Callback for when the request completes
+        request.onload = function(){
+          let theActualData = JSON.parse(request.response).series[0].data
+
+          drawBasic(theActualData)
+        }
+        // Callback for when there's an error
+        request.error = function(err){
+          console.log("error is: ", err)
+        }
+        // Send the request to the specified URL
+        request.send()
+      }
+
 function drawComparingChart() {
       var data = new google.visualization.DataTable();
       data.addColumn('string', 'Year');
